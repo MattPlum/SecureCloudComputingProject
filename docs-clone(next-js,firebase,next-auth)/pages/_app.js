b0eccb00a7dc65da8196cfc5b12b7f7f0c3794ac
Arun Misir <70327489+amisir0219@users.noreply.github.com>
2021-11-01 12:28:01 -0400

    Update _app.js

--- docs-clone(next-js,firebase,next-auth)/pages/_app.js
+++ docs-clone(next-js,firebase,next-auth)/pages/_app.js
@@ -1,8 +1,11 @@
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
 import "@material-tailwind/react/tailwind.css";
 import Head from 'next/head';
+import {  getSession} from "next-auth/client";
+import {Provider} from "next-auth/client";
+import "../styles.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: {session, ...pageProps} }) {
   return (
   <>
   <Head>
@@ -11,9 +14,22 @@ function MyApp({ Component, pageProps }) {
      rel="stylesheet"
     />
   </Head>
+  <Provider session={session}>
   <Component {...pageProps} />
+  </Provider>
   </>
   )
+  
 }
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
 
 export default MyApp
