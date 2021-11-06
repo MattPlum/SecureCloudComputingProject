789023c2393d89609da05c751dc68dd1e4bf51b7
Arun Misir <70327489+amisir0219@users.noreply.github.com>
2021-11-06 00:34:08 -0400

    Update [...nextauth].js

--- NextJS(Version)/pages/api/auth/[...nextauth].js
+++ NextJS(Version)/pages/api/auth/[...nextauth].js
@@ -8,8 +8,8 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId:'846189577544-7uu11h1mft3poeq9rsaj6b35dd0tuh95.apps.googleusercontent.com',//process.env.GOOGLE_CLIENT_ID,
-      clientSecret:'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',//process.env.GOOGLE_CLIENT_SECRET,
+      clientId:process.env.GOOGLE_CLIENT_ID,
+      clientSecret:process.env.GOOGLE_CLIENT_SECRET,
     }),
     // ...add more providers here
 
