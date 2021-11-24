62a2198a4d3fd318e15b68290f2672dc812ecf07
Arun Misir <70327489+amisir0219@users.noreply.github.com>
2021-11-24 00:59:39 -0500

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA5nZBkpAKtvjuWRX1QExgiQjO0VGgbfcA",
-  authDomain: "gdocs-clone-e64ce.firebaseapp.com",
-  projectId: "gdocs-clone-e64ce",
-  storageBucket: "gdocs-clone-e64ce.appspot.com",
-  messagingSenderId: "846189577544",
-  appId: "1:846189577544:web:c4a556049acce5f5989f7d"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
   };
   
 
