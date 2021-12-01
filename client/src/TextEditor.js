1c48f8296a6af2ddee73ec19e814629c4b517eae
amisir0219 <amisir001@citymail.cuny.edu>
2021-11-30 19:12:26 -0500

    Development Environment
    Original code for development

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -23,7 +23,7 @@ export default function TextEditor() {
   const [quill, setQuill] = useState()
 
   useEffect(() => {
-    const s = io("https://backend-amisir0219.cloud.okteto.net")
+    const s = io("http://localhost:3001")
     setSocket(s)
 
     return () => {
