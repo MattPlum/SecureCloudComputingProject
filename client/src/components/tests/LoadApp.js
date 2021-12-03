7d3f2fc18f1cbf1a40e5abbbefc1c337a8cf7a5c
mattplum <mattdouglee@gmail.com>
2021-12-03 17:51:41 -0500

    modified loadapp

---
+++ client/src/components/tests/LoadApp.js
@@ -0,0 +1,56 @@
+
+
+const {Builder, By, Key, until} = require('selenium-webdriver');
+var chrome = require("selenium-webdriver/chrome");
+
+var options = new chrome.Options();
+options.addArguments('--disable-web-security');
+options.addArguments('--allow-running-insecure-content');
+//options.addArguments('--headless');
+options.addArguments("--user-data-dir=C:\\Users\\leema\\AppData\\Local\\Google\\Chrome\\User Data\\Default")
+options.addArguments("--profile-directory=Profile 2");
+
+var driver =  new Builder()
+    .forBrowser("chrome").setChromeOptions(options).build();
+
+
+async function LoadApplication(){
+     try
+     {
+        //Load application
+        await driver.get("http://localhost:3000/");
+        console.log("Test 1: Application Loaded - PASS")
+     }
+     catch(e)
+     {
+        console.log("Test 1: Application Loaded - FAIL\n",e)
+
+     }
+ }
+
+ async function ClickButton(){
+    try 
+    {
+        await driver.findElement(By.xpath("/html/body/div/div/button")).click();    //click the sign in button
+        console.log("Test 1: ClickButton - PASS")
+
+    } catch (error) {
+        console.log("Test 2: ClickButton - FAIL\n",error)
+
+    }
+ }
+
+
+ 
+// var driver = new webdriver.Builder().
+//    withCapabilities(webdriver.Capabilities.chrome()).
+//    build();
+
+try{
+    LoadApplication();
+    ClickButton();
+}
+catch(error)
+{
+    console.log("test failed: " + error)
+}
