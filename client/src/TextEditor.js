0a81592e01dbe16c1742da783bf65e61e55017e3
Arun Misir <70327489+amisir0219@users.noreply.github.com>
2021-12-03 02:01:33 -0500

    Update TextEditor.js

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -23,7 +23,7 @@ export default function TextEditor() {
   const [quill, setQuill] = useState()
 
   useEffect(() => {
-    const s = io("https://backen-amisir0219.cloud.okteto.net")
+    const s = io("https://backend-amisir0219.cloud.okteto.net")
     setSocket(s)
 
     return () => {
